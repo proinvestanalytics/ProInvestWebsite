e253574168922246569cf97285b7977c08fe9d85
salzo6 <59977178+salzo6@users.noreply.github.com>
2025-04-27 18:05:52 -0400

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Logo: React.FC = () => {
   return (
     <div className="flex items-center">
-      <img 
-        src="/proinvest-logo.png" 
-        alt="ProInvest Logo" 
-        className="h-12 w-auto"
-      />
+      <Link to="/">
+        <img 
+          src="/proinvest-logo.png" 
+          alt="ProInvest Logo" 
+          className="h-12 w-auto rounded-lg shadow-lg"
+        />
+      </Link>
     </div>
   );
 };
